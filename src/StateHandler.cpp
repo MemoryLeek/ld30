198389ff4448167676f2f0138884c8355cd51111
Znurre <znurree@gmail.com>
2014-08-22 06:44:24 +0200

    Improved state handling with key input delegation

---
+++ src/StateHandler.cpp
@@ -0,0 +1,5 @@
+#include "StateHandler.h"
+
+StateHandler::StateHandler()
+{
+}
---
+++ src/StateHandler.h
@@ -0,0 +1,10 @@
+#ifndef STATEHANDLER_H
+#define STATEHANDLER_H
+
+class StateHandler
+{
+	public:
+		StateHandler();
+};
+
+#endif // STATEHANDLER_H
